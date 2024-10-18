47b2e785067b29aa7fa5ccd6546c2161bfb418ac
Joe Shoulak <joepshoulak@me.com>
2024-10-18 16:28:08 -0500

    quarter note

--- scene.py
+++ scene.py
@@ -1,32 +1,46 @@
 from manim import *
 
+# line thicknesses
+THIN = 1.25
+THICK = 3
+STEM = 4
+
+def quarterNote(noteLineSpacing):
+  head = Ellipse(1.618, 1, color=WHITE).set_opacity(1)
+  head.rotate(22 * DEGREES).scale_to_fit_height(noteLineSpacing)
+
+  stem = Line([0, 0, 0], [0, noteLineSpacing*3, 0], stroke_width=STEM)
+  stem.align_to(head, DR).shift(UP*(2/3)*noteLineSpacing)
+
+  return VGroup(head, stem).center()
+
 def musicStaff(signature=[4,4]):
-  THIN = 1.25
-  THICK = 2.5
-
   # note lines
-  noteLines = [Line(LEFT, RIGHT, stroke_width=THIN) for i in range(5)]
+  nLines = [Line(LEFT, RIGHT, stroke_width=THIN) for i in range(5)]
 
-  for i in range(1, len(noteLines)):
-      noteLines[i].next_to(noteLines[i-1], DOWN)
+  for i in range(1, len(nLines)):
+      nLines[i].next_to(nLines[i-1], DOWN)
+
+  nLineSpacing = nLines[0].get_start()[1] - nLines[1].get_start()[1]
 
   # bar lines
   barLines = [
-    Line(noteLines[0].get_start(), noteLines[4].get_start(), stroke_width=THIN), 
-    Line(noteLines[0].get_end(), noteLines[4].get_end(), stroke_width=THIN).shift(LEFT*0.125), 
-    Line(noteLines[0].get_end(), noteLines[4].get_end(), stroke_width=THICK)
+    Line(nLines[0].get_start(), nLines[4].get_start(), stroke_width=THIN), 
+    Line(nLines[0].get_end(), nLines[4].get_end(), stroke_width=THIN).shift(LEFT*0.125), 
+    Line(nLines[0].get_end(), nLines[4].get_end(), stroke_width=THICK)
   ]
 
   # time signature
-  numHeight = noteLines[0].get_start()[1] - noteLines[2].get_start()[1]
-  signature = [MathTex(signature[i]).scale_to_fit_height(numHeight) for i in [0, 1]]
-  signature[0].align_to(noteLines[0].get_start(), UL)
+  signature = [MathTex(signature[i]).scale_to_fit_height(2*nLineSpacing) for i in [0, 1]]
+  signature[0].align_to(nLines[0].get_start(), UL)
   signature[1].next_to(signature[0], DOWN, buff=0)
 
-  return VGroup(*noteLines, *barLines, *signature)
+  note = quarterNote(nLineSpacing).align_to(nLines[3], DOWN)
+
+  return VGroup(*nLines, *barLines, *signature, note).center()
 
 class Music(Scene):
   def construct(self):
-    staff = musicStaff()
+    staff = musicStaff().scale(3)
 
     self.add(staff)
