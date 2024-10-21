5d805c14cc097001715df50563b19cb065098af3
Joe Shoulak <joepshoulak2@gmail.com>
2024-10-21 15:39:10 -0500

    polish

--- scenes/Rabbits.py
+++ scenes/Rabbits.py
@@ -1,25 +1,22 @@
 from manim import *
 from math import sqrt
 
-rabbit_path = "rabbit.svg"
-
 PHI = (1+sqrt(5))/2
-RABBIT_SIZE = 1
+RABBIT_SIZE = 2
+rabbit_path = "rabbit.svg"
+rabbitSVG = SVGMobject(rabbit_path, height=RABBIT_SIZE, fill_color=WHITE, stroke_color=BLACK, stroke_width=5, stroke_opacity=1)
 
-def rabbitsSvg():
-    rabbit = SVGMobject(rabbit_path, height=RABBIT_SIZE, fill_color=WHITE, stroke_color=BLACK, stroke_width=5, stroke_opacity=1)
-    return VGroup(
-        rabbit.copy().scale(0.9),
-        rabbit.copy().shift(DOWN*0.1*RABBIT_SIZE, LEFT*0.2*RABBIT_SIZE),
-    )
-    
+# Helper classes
 class Rabbit:
     def __init__(self, adult=False):
         self.adult = adult
         self.image = self.get_image()
 
     def get_image(self):
-        image = rabbitsSvg()
+        image = VGroup(
+            rabbitSVG.copy().scale(0.9),
+            rabbitSVG.copy().shift(DOWN*0.1*RABBIT_SIZE, LEFT*0.2*RABBIT_SIZE),
+        )
         if not self.adult: image.scale(1/PHI)
         return image
     
@@ -77,39 +74,66 @@ class RabbitTree:
 
         self.iterate(n-1)
 
+# Main
 class Rabbits(MovingCameraScene):
     def construct(self):
+        # Setup
         scene_width = config.frame_width
         scene_height = config.frame_height 
 
         tree = RabbitTree()
-        tree.iterate(4)
+        tree.iterate(9)
         allRows = VGroup()
 
-        def center_camera():
-            return self.camera.frame.animate.move_to(allRows.get_center())
+        # Helpers
+        def update_camera():
+            anim = self.camera.frame.animate.move_to(allRows.get_center())
 
-        def animateIteration(n, allRows):
+            if allRows.width > scene_width - 1:
+                return anim.scale_to_fit_width(allRows.width + 1)
+            if allRows.height > scene_height - 1:
+                return anim.scale_to_fit_height(allRows.height + 1)
+
+            return anim
+
+        def animateIteration(n, allRows, skip_animation=False):
             allRows += tree.get_generation_as_vgroup(n).next_to(allRows[-1], DOWN)
+            if skip_animation: return
+
             i = 0
             for r, mob in zip(tree.get_generation(n-1), allRows[-2].submobjects):
                 if r.data.adult:
-                    print("yes")
-                    self.play(Transform(VGroup(mob.copy(), mob.copy()), allRows[-1][i:i+2]), center_camera())
+                    self.play(Transform(VGroup(mob.copy(), mob.copy()), allRows[-1][i:i+2]), update_camera())
+                    self.wait()
                     i += 2
                 else:
-                    print("no")
-                    self.play(Transform(VGroup(mob.copy()), allRows[-1][i]), center_camera())
+                    self.play(Transform(VGroup(mob.copy()), allRows[-1][i]), update_camera())
+                    self.wait()
                     i += 1
 
+        # Layout
         allRows += tree.get_generation_as_vgroup(1)
         self.play(Write(allRows[0]))
 
-        print("it start")
         animateIteration(2, allRows)
-        print("it start")
         animateIteration(3, allRows)
-        print("it start")
         animateIteration(4, allRows)
-        print("it start")
         animateIteration(5, allRows)
+        animateIteration(6, allRows, skip_animation=True)
+        animateIteration(7, allRows, skip_animation=True)
+        animateIteration(8, allRows, skip_animation=True)
+        animateIteration(9, allRows, skip_animation=True)
+
+        tempH, tempC = self.camera.frame.height, self.camera.frame.get_center()
+        self.play(update_camera(), FadeIn(allRows[5:]))
+        self.wait()
+
+        self.play(self.camera.frame.animate.scale_to_fit_height(tempH).move_to(tempC), FadeOut(allRows[5:]))
+        self.wait()
+
+        # Animation
+        for i in range(3):
+            self.play(allRows[i+1].animate.set_fill_color(RED), allRows[i+2][:len(allRows[i+1])].animate.set_fill_color(RED))
+            self.play(allRows[i].animate.set_fill_color(YELLOW_D), allRows[i+2][len(allRows[i+1]):].animate.set_fill_color(YELLOW_D))
+            self.play(allRows[:5].animate.set_fill_color(WHITE))
+            self.wait()
--- test.py
+++ test.py
@@ -2,9 +2,8 @@ from manim import *
 
 class Test(Scene):
   def constrcut(self):
-    s = Square()
-    s.foo = True
-    print(s.foo)
-    self.add(s)
-    if s.foo:
-      print("Test")
+    t = TexTemplate()
+    t.add_to_preamble(r"\usepackage(wasysym)")
+
+    m = MathTex(r"\male", tex_template=t)
+    self.add(m)
