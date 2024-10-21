c75864352dfcca1587b83e21ec10a7659e23829b
Joe Shoulak <joepshoulak2@gmail.com>
2024-10-21 00:06:14 -0500

    major progress

--- scenes/Rabbits.py
+++ scenes/Rabbits.py
@@ -1,8 +1,126 @@
 from manim import *
+from math import sqrt
 
-class Rabbits(Scene):
+rabbit_path = "rabbit.svg"
+
+PHI = (1+sqrt(5))/2
+RABBIT_SIZE = 2
+
+def rabbitsSvg():
+    rabbit = SVGMobject(rabbit_path, height=RABBIT_SIZE, fill_color=WHITE, stroke_color=BLACK, stroke_width=5, stroke_opacity=1)
+    return VGroup(
+        rabbit.copy().scale(0.9),
+        rabbit.copy().shift(DOWN*0.1*RABBIT_SIZE, LEFT*0.2*RABBIT_SIZE),
+    )
+    
+class Rabbit:
+    def __init__(self, adult=False):
+        self.adult = adult
+        self.image = self.get_image()
+
+    def get_image(self):
+        image = rabbitsSvg()
+        if not self.adult: image.scale(1/PHI)
+        return image
+    
+class NoRoomForChildren(Exception):
+    def __str__(self): "No room for child in node"
+    
+class RabbitTree:
+    def __init__(self):
+        self.left: RabbitTree = None
+        self.right: RabbitTree = None
+        self.data = Rabbit()
+
+    def get_generation(self, n):
+        if n == 1: return [self]
+        
+        generation = []
+        if self.left: generation += self.left.get_generation(n - 1)
+        if self.right: generation += self.right.get_generation(n - 1)
+        return generation
+
+    def get_leaves(self):
+        if self.is_leaf():
+            return [self]
+        
+        leaves = []
+        if self.left: leaves += [*self.left.get_leaves()]
+        if self.right: leaves += [*self.right.get_leaves()]
+        return leaves
+
+    def is_leaf(self):
+        return not self.left and not self.right
+
+    def get_children(self):
+        children = []
+        if self.left: children.append(self.left)
+        if self.right: children.append(self.right)
+        return children
+    
+    def add_child(self, adult=False):
+        if self.left and self.right: raise NoRoomForChildren
+        branch = "right" if self.left else "left"
+        setattr(self, branch, RabbitTree())
+        getattr(self, branch).data = Rabbit(adult)
+    
+    def iterate(self, n=1):
+        if not n: return
+
+        for leaf in self.get_leaves():
+            leaf.add_child(adult=True)
+            if leaf.data.adult:
+                leaf.add_child()
+
+        self.iterate(n-1)
+
+def get_next_gen_mobs(row):
+    copies = []
+    for rabbit in row:
+        if rabbit.height < Rabbit().get_image().height:
+            copies += [rabbit.copy(), rabbit.copy()]
+        else:
+            copies += rabbit.copy()
+
+    return copies
+
+class Rabbits(MovingCameraScene):
     def construct(self):
-        self.add(Square())
+        scene_width = config.frame_width
+        scene_height = config.frame_height 
+
+        tree = RabbitTree()
+
+        iterations = 4
+
+        tree.iterate(iterations)
+
+        allRows = VGroup()
+
+        def camera_updater_force():
+            anim = self.camera.frame.animate.move_to(allRows.get_center())
+                
+            if allRows.height > scene_height - 1:
+                anim.scale_to_fit_height(allRows.height+1)
+
+            return anim
+        
+        def camera_updater(mobj):
+            mobj.move_to(allRows.get_center())
+
+        rabbits = tree.get_generation(1)
+        mobjs = VGroup(*[r.data.image for r in rabbits])
+        allRows += mobjs
+        self.play(Write(mobjs, stroke_color=WHITE))
+        self.wait()
+
+        for i in range(1, iterations+1):
+            rabbits = tree.get_generation(i+1)
+            mobjs = VGroup(*[r.data.image for r in rabbits]).arrange(RIGHT).next_to(allRows[-1], DOWN)
+            allRows += mobjs
+            self.play(Transform(VGroup(*get_next_gen_mobs(allRows[-2])), mobjs), camera_updater_force())
+            self.wait()
 
         self.wait()
-        
+
+# Fix the gact that it's not always adult rabbits that spawn off a pair
--- test.py
+++ test.py
@@ -1,42 +1,19 @@
 from manim import *
-from music_score import *
-from music_score import NoteTypes as NT
 
-# class MeasureTest(Scene):
-#   def construct(self):
-    # m1 = Measure([NT.HALF, NT.QUARTER, NT.QUARTER]).scale(0.3).shift(LEFT * 4)
-    # m2 = Measure([NT.QUARTER, NT.HALF, NT.QUARTER]).scale(0.3).shift(LEFT * 4)
-    # m3 = Measure([NT.QUARTER, NT.QUARTER, NT.HALF]).scale(0.3).shift(LEFT * 4)
-    # m4 = Measure([NT.HALF, NT.HALF]).scale(0.3).shift(LEFT * 4)
-    # m5 = Measure([NT.QUARTER, NT.QUARTER, NT.QUARTER, NT.QUARTER]).scale(0.3).shift(LEFT * 4)
-    # m6 = Measure([]).scale(0.3).shift(LEFT * 4)
-
-    # ms = VGroup(m1, m2, m3, m4, m5, m6).arrange(DOWN).center().scale_to_fit_height(7)
-    # self.add(ms)
-
-    # self.add(Measure([NT.WHOLE]).scale(3))
-    # self.add(Measure([NT.QUARTER, NT.QUARTER, NT.QUARTER, NT.QUARTER]).scale(3))
-    # self.add(Measure([NT.QUARTER, NT.HALF, NT.QUARTER]).scale(3))
-    # self.add(Measure([NT.QUARTER, NT.QUARTER], signature=[4, 8]).scale(3))
-    # self.add(Measure([NT.QUARTER, NT.QUARTER, NT.QUARTER, NT.QUARTER], signature=[8, 8]).scale(3))
-
-# class Test(Scene):
-#   def construct(self):
-#     t = Tex(r"\genfrac{}{}{0pt}{}{1}{2}")
-#     self.add(Write(t))
-
-class Test2(Scene):
+class Test(MovingCameraScene):
   def construct(self):
-    # t = Tex("N", "+", "N", "=", "N")
-    # self.add(t)
-    
-    # self.play(
-    #   t[0].animate.become(Triangle().move_to(t[0].get_center()).scale_to_fit_width(t[0].width)),
-    #   t[2].animate.become(Square().move_to(t[2].get_center()).scale_to_fit_width(t[2].width)),
-    #   t[4].animate.become(Circle().move_to(t[4].get_center()).scale_to_fit_width(t[4].width))
-    # )
+    squares = VGroup(Square(1))
 
-    definition = MathTex(r"\text{\# of notes from\ } [", "|||", ",", "|||", r"] \text{\ in a measure of\ } \frac{n}{4}: F_{n+1}", font_size=55).to_edge(UP)
-    definition[1].become(QuarterNote(0.2).move_to(definition[1].get_center()))
-    definition[3].become(HalfNote(0.2).move_to(definition[3].get_center()))
-    self.play(Write(definition))
+    def cameraUpdater(mobj):
+      mobj.move_to(squares.get_center())
+
+    def makeItMove(mobj):
+      return mobj.move_to(squares.get_center())
+
+    # self.camera.frame.add_updater(cameraUpdater)
+
+    self.play(Write(squares[0]))
+
+    for i in range(5):
+      squares += Square(1).next_to(squares[-1], DOWN)
+      self.play(Write(squares[-1]), makeItMove(self.camera.frame.animate))
